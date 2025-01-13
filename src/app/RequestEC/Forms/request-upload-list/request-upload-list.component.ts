2d7ca6cec69341ce5ad121a5efd9fc5839fdd974
andlimhot <andi@ppu.co.id>
2025-01-13 15:28:17 +0700

    update 20250113 - by am

--- src/app/RequestEC/Forms/request-upload-list/request-upload-list.component.ts
+++ src/app/RequestEC/Forms/request-upload-list/request-upload-list.component.ts
@@ -11,6 +11,7 @@ import { RequestServService } from '../../Services/request-serv.service';
 import { productlist } from 'src/app/MasterApps/Models/productlist';
 import { producttypelist } from 'src/app/MasterApps/Models/productypelist';
 import { ProductProducttypeServService } from 'src/app/MasterApps/Services/product-producttype-serv.service';
+import { RequestDtl } from '../../Models/RequestDtl';
 
 @Component({
   selector: 'app-request-upload-list',
@@ -24,10 +25,68 @@ export class RequestUploadListComponent implements OnInit {
 
   ReQForm: FormGroup;
   preview='';
+  preview2='';
+  preview3='';
+  preview4='';
+  coreTransRequestEcDtl: RequestDtl[] = [];
   prodlist:productlist[]=[];
   prodtylist:producttypelist[]=[];
   selectedprod:string="";
   selectedprodtype:string="";
+  selectedFiles: File[] = [];
+
+  userid: string = 'USer01';
+  data: RequestDtl = { 
+    ctecdCtechId: "",
+    ctecdId: "",
+    ctecdProductCode: "",
+    ctecdProductName: "",
+    ctecdProducttypeCode: "",
+    ctecdProducttypeName: "",
+    ctecdProducttypeStockQty: 0,
+    ctecdProducttypeMinQty: 0,
+    ctecdProducttypePrice: 0,
+    ctecdProducttypeRangeQty1: 0,
+    ctecdProducttypeRangePrice1: 0,
+    ctecdProducttypeRangeQty2: 0,
+    ctecdProducttypeRangePrice2: 0,
+    ctecdProducttypeDesc: "",
+    ctecdProductTypeSize: "",
+    ctecdProductTypeSpec: "",
+    ctecdProductTypeAlias: "",
+    ctecdProdTypeImg1Filename: "",
+    ctecdProdTypeImg1Filepath: "",
+    ctecdProdTypeImg2Filename: "",
+    ctecdProdTypeImg2Filepath: "",
+    ctecdProdTypeImg3Filename: "",
+    ctecdProdTypeImg3Filepath: "",
+    ctecdProdTypeImg4Filename: "",
+    ctecdProdTypeImg4Filepath: "",
+    ctecdNewUsed: "",
+    ctecdStatus: "",
+    ctecdReason: "",
+    ctecdCreateBy: "",
+    ctecdCreateDate: "",
+    ctecdUpdateBy: "",
+    ctecdUpdateDate: "",
+    ctecdBestPrice: "",
+    ctecdBestProduct: "",
+    ctecdNewProduct: "",
+    ctecdSale: "",
+    ctecdImg1CtpicRefNo: "",
+    ctecdImg1CtpicSeqNo: "",
+    ctecdImg2CtpicRefNo: "",
+    ctecdImg2CtpicSeqNo: "",
+    ctecdImg3CtpicRefNo: "",
+    ctecdImg3CtpicSeqNo: "",
+    ctecdImg4CtpicRefNo: "",
+    ctecdImg4CtpicSeqNo: ""
+  };
+  file1: any = null;
+  file2: any = null;
+  file3: any = null;
+  file4: any = null;
+  requestNumber: string = '';
 
   constructor(private reqServ : RequestServService, private formBuider: FormBuilder, private masterserv: ProductProducttypeServService){
     this.ReQForm = this.formBuider.group({
@@ -75,7 +134,7 @@ export class RequestUploadListComponent implements OnInit {
     ctecdImg3CtpicSeqNo: "",
     ctecdImg4CtpicRefNo: "",
     ctecdImg4CtpicSeqNo: ""
-    })
+    });
   }
 
 
@@ -85,7 +144,6 @@ export class RequestUploadListComponent implements OnInit {
 
   changeproduct() {
     this.prodtylist  =[];    
-    alert(this.selectedprod);
     this.getProducttypeList(this.selectedprod);
     
   }
@@ -93,14 +151,53 @@ export class RequestUploadListComponent implements OnInit {
   getProductList(){
      this.prodtylist = [];
       this.masterserv.getProductList().subscribe((res: productlist[]) => {
-        this.prodlist = res;  
+        this.prodlist = res; 
       });
   }
 
   getProducttypeList(value: any){  
      this.prodtylist = [];
+     
       this.masterserv.getProductTypeList(value).subscribe((res: producttypelist[]) => {
         this.prodtylist = res;        
       });
+      
+  }
+
+  onFileSelected(event: any, fileNumber: number) {
+    switch (fileNumber) {
+      case 1:
+        this.file1 = event.target.files[0];
+        break;
+      case 2:
+        this.file2 = event.target.files[0];
+        break;
+      case 3:
+        this.file3 = event.target.files[0];
+        break;
+      case 4:
+        this.file4 = event.target.files[0];
+        break;
+    }
+  }
+
+
+  submitRequest() {
+    this.reqServ.createReqWeb(this.userid, this.data, this.file1, this.file2, this.file3, this.file4)
+      .subscribe(
+        response => {
+          this.requestNumber = response;
+          console.log('Request berhasil dibuat dengan nomor:', this.requestNumber);
+          // Tambahkan logika untuk menangani response sukses, misalnya:
+          // - Reset form
+          // - Tampilkan pesan sukses
+          // - Redirect ke halaman lain
+        },
+        error => {
+          console.error('Terjadi kesalahan:', error);
+          // Tambahkan logika untuk menangani error, misalnya:
+          // - Tampilkan pesan error
+        }
+      );
   }
 }
--- src/app/RequestEC/Services/request-serv.service.ts
+++ src/app/RequestEC/Services/request-serv.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RequestDtl } from '../Models/RequestDtl';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestServService {
+  curDate=new Date();
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllReqEc(): Observable<RequestDtl[]> {
+    return this.http.get<RequestDtl[]>("http://localhost:8091/wc-svc/webcust/getCtecdListAll");
+  }
+
+  getReqEcById(id: number): Observable<RequestDtl> {
+    return this.http.get<RequestDtl>("http://localhost:8091/wc-svc/webcust/getCtecdListByCtechId?CtechId="+id);
+  }
+
+  createReqWeb(userid: string, data: RequestDtl, file1: File, file2: File, file3: File, file4: File): Observable<string> {
+    const formData = new FormData();
+    formData.append('userid', userid);
+    formData.append('data', new Blob([JSON.stringify(data)], { type: 'application/json' }));
+
+    if (file1) { formData.append('file1', file1); }
+    if (file2) { formData.append('file2', file2); }
+    if (file3) { formData.append('file3', file3); }
+    if (file4) { formData.append('file4', file4); }
+
+    return this.http.post<string>('http://localhost:8091/wc-svc/webcust/saveupdreqecdtl', formData);
+  }
+
+/*
+  updateProduct(id: number, product: Product, files: FileList): Observable<Product> {
+    const formData = new FormData();
+    formData.append('name', product.name);
+    formData.append('description', product.description);
+    formData.append('price', String(product.price));
+    formData.append('custNo', product.custNo);
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        formData.append('files', files[i]);
+      }
+    }
+    return this.http.put<Product>(`${this.baseUrl}/${id}`, formData);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+  */
 }
